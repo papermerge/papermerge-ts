cac1b44754031a7f3c8cd9cde557886f6df0b3b6
Eugen Ciur <eugen@papermerge.com>
2023-04-22 07:26:36 +0200

    Login component moved to auth-server

--- src/components/login/logo.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./logo.module.scss";
-
-export default function Logo() {
-  return (
-    <div className={styles.logo}>
-      <img width="112px" height="108px" src="/images/logo.svg" />
-    </div>
-  );
-}
